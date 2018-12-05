8b90f64c1dff8160d61182c6ec82f390712132ab
etc <trashbxg@gmail.com>
2018-12-05 04:10:08 +0100

    oopfs

--- README.md
+++
@@ -1,3 +0,0 @@
-# [etcroot.pw](https://etcroot.pw/)
-
-Making use of [TannerReynolds/node-sharex-server](https://github.com/TannerReynolds/node-sharex-server) on my site.
---
+++ js/index.js
@@ -0,0 +1,5 @@
+var typed = new Typed('.typed', {
+  strings: ["Web Developer", "Bot Creator", "Web Designer", "Filthy Weaboo", "Secretly Koreaboo"],
+  typeSpeed: 80,
+  contentType: 'html'
+});
